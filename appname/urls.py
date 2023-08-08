5eee539ff871b13c6ae05a0dd72feef1d503b4b5
saitejagandavarapu <141764551+saitejagandavarapu@users.noreply.github.com>
2023-08-08 10:52:21 -0700

    Add files via upload

---
+++ appname/urls.py
@@ -0,0 +1,41 @@
+"""
+URL configuration for projectname project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+
+from appname import views
+
+from appname.views import StudentReg, Studentlist ,Studentdetail , StudentUpdate, StudentDelete 
+
+app_name='appname'
+
+urlpatterns = [
+    path('home', views.home, name = "home"),
+    path('product', views.product, name = "product"),
+    path('service', views.service, name = "service"),
+    path('contact', views.contact, name = "contact"),
+    path('aboutus', views.about, name = "aboutus"),
+    path('students', views.students, name = "students"),
+    
+
+   path('studentreg', StudentReg.as_view(), name = 'studentreg'),
+   path('<pk>/studentdetail', Studentdetail.as_view(), name = 'studentdetail'),
+   path('',Studentlist.as_view(), name = 'studentlist'),
+   path('<pk>/studentupdate',StudentUpdate.as_view(),name = 'studentupdate'),
+   path('<pk>/studentdelete', StudentDelete.as_view(), name = 'studentdelete'),
+
+
+]
---
+++ appname/views.py
@@ -0,0 +1,56 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+def product(request):
+    return render(request,'product.html')
+
+def service(request):
+    return render(request,'service.html')
+
+def contact(request):
+    return render(request,'about.html')
+
+def about(request):
+    return render(request,'contact.html')
+
+
+from appname.models import Student
+
+def students(request):
+    student = Student.objects.all()
+    context = {student:'student'}
+    return render(request, 'Student.html',context)
+
+
+#class based views
+
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView, ListView
+
+class StudentReg(CreateView):
+	model = Student
+	fields = '__all__'
+	template_name = 'CBV/studentreg.html'
+	success_url ="/"
+
+class Studentlist(ListView):
+	model = Student
+	template_name = 'CBV/studentlist.html'
+
+class Studentdetail(DetailView):
+	model =Student
+	template_name = 'CBV/studentdetail.html'
+
+class StudentUpdate(UpdateView):
+	model = Student
+	fields = "__all__"
+	template_name = 'CBV/studentupdate.html'
+	success_url ="/"
+
+
+class StudentDelete(DeleteView):
+	model = Student
+	template_name = 'CBV/studentdelete.html'
+	success_url ="/"
